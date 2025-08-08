903f40ec5fdbbfc836f3623c45320ffdf9f258e4
elecun <elecun2@gamil.com>
2025-08-08 12:34:59 +0900

    save with normal vector

--- tools/stl_to_pcd.py
+++ tools/stl_to_pcd.py
@@ -14,19 +14,17 @@ if __name__ == "__main__":
     console = ConsoleLogger.get_logger()
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--file', nargs='?', required=True, help="Polygon File Format(*.ply)", default="default.ply")
+    parser.add_argument('--file', nargs='?', required=True, help="Mesh File (*.stl)", default="")
     parser.add_argument('--sample', nargs='?', required=False, help="Number of Samples", default=10000)
     parser.add_argument('--ext', nargs='?', required=False, help="Converting File (PCD, PLY)", default="pcd")
     args = parser.parse_args()
 
     try:
-        mesh = trimesh.load(args.file)
-        points = mesh.sample(int(args.sample))
-        pcd = o3d.geometry.PointCloud()
-        pcd.points = o3d.utility.Vector3dVector(points)
+        mesh = o3d.io.read_triangle_mesh(args.file)
+        mesh.compute_vertex_normals()
+        pcd = mesh.sample_points_uniformly(number_of_points=int(args.sample))
 
         path_no_ext, ext = os.path.splitext(args.file)
-        o3d.io.write_point_cloud(f"{path_no_ext}.{args.ext}", pcd)
         o3d.io.write_point_cloud(f"{path_no_ext}.{args.ext}", pcd)
 
     except Exception as e:
